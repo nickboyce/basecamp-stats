4ce3d10c9a4c28b843a481076e54487a6637c8fd
Nick Boyce <nick@nickboyce.com>
2014-06-28 14:49:48 +0100

    get full person data on init

--- public/static/js/models/person.js
+++ public/static/js/models/person.js
@@ -2,4 +2,13 @@ var BCX = BCX || {};
 
 BCX.Person = Backbone.Model.extend({
 
+  url: function () {
+    return "/proxy/people/" + this.id + ".json"
+  },
+
+  initialize: function () {
+    if (!this.outstanding_todos) {
+      this.fetch();
+    }
+  }
 });
